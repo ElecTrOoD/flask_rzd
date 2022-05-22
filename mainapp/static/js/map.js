36165dbd9699b5f43997f00e5d619307019519e8
ElecTrOoD <32058883+ElecTrOoD@users.noreply.github.com>
2022-05-22 16:39:31 +0300

    gmaps replaced with ymaps

---
+++ mainapp/static/js/map.js
@@ -0,0 +1,29 @@
+ymaps.ready(init);
+
+function init() {
+    let initialCoords = [52.033428, 113.500911];
+    if (users) {
+        if (users.length === 1) {
+            initialCoords = [users[0].geo_lat, users[0].geo_long];
+        }
+    }
+
+    $("#placeholder").remove()
+
+    let map = new ymaps.Map("map", {
+        center: initialCoords,
+        zoom: 13
+    });
+
+    for (let user of users) {
+        let newUserPlacemark = new ymaps.Placemark([user.geo_lat, user.geo_long], {
+            balloonContentHeader: user.first_name + ' ' + user.last_name,
+            balloonContentBody: normalize_date(user.geo_updated_at),
+        }, {
+            preset: 'islands#redRailwayIcon'
+        });
+
+        map.geoObjects.add(newUserPlacemark);
+
+    }
+}
